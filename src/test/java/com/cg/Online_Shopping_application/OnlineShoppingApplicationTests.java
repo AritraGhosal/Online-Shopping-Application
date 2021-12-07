d5ae9e7f280e893b51cb019048c4200addd9d3eb
AritraGhosal <aritra.ghosal.96@gmail.com>
2021-12-08 00:29:16 +0530

    Initial commit

---
+++ src/test/java/com/cg/Online_Shopping_application/OnlineShoppingApplicationTests.java
@@ -0,0 +1,15 @@
+package com.cg.Online_Shopping_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.cg.onlineshopping_application.OnlineshoppingApplication;
+
+@SpringBootTest(classes = OnlineshoppingApplication.class)
+class OnlineShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
+
