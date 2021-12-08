0ae6059138526ed2fffc95acb78f4eb51e439b88
AritraGhosal <aritra.ghosal.96@gmail.com>
2021-12-08 09:22:39 +0530

    small changes

--- src/main/java/com/cg/onlineshopping_application/OnlineshoppingApplication.java
+++ src/main/java/com/cg/onlineshopping_application/OnlineshoppingApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OnlineshoppingApplication {
-//main method
 	public static void main(String[] args) { 
-		SpringApplication.run(OnlineshoppingApplication.class, args); //run the spring framework
+		SpringApplication.run(OnlineshoppingApplication.class, args); 
 	}
 }
--- src/main/java/com/cg/onlineshopping_application/config/SwaggerConfig.java
+++ src/main/java/com/cg/onlineshopping_application/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
     @Bean
-    public Docket api(){ //bean -> an object of Docket
+    public Docket api(){ 
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.cg.onlineshopping_application"))
